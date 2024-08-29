7effcdde15e8109f736acddcf8c5c1c49cb2bdd5
JacksoonHayes <jackson-hayes@live.nmit.ac.nz>
2024-08-29 12:28:39 +1200

    test

--- DAT602_Project/AdminForm.Designer.cs
+++ DAT602_Project/AdminForm.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.playersListBox = new System.Windows.Forms.ListBox();
+            this.gamesListBox = new System.Windows.Forms.ListBox();
+            this.playersLabel = new System.Windows.Forms.Label();
+            this.gamesLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // playersListBox
+            // 
+            this.playersListBox.FormattingEnabled = true;
+            this.playersListBox.ItemHeight = 16;
+            this.playersListBox.Items.AddRange(new object[] {
+            "John",
+            "Jane",
+            "Jim"});
+            this.playersListBox.Location = new System.Drawing.Point(12, 57);
+            this.playersListBox.Name = "playersListBox";
+            this.playersListBox.Size = new System.Drawing.Size(130, 212);
+            this.playersListBox.TabIndex = 0;
+            // 
+            // gamesListBox
+            // 
+            this.gamesListBox.FormattingEnabled = true;
+            this.gamesListBox.ItemHeight = 16;
+            this.gamesListBox.Items.AddRange(new object[] {
+            "John\'s Game (1)"});
+            this.gamesListBox.Location = new System.Drawing.Point(148, 57);
+            this.gamesListBox.Name = "gamesListBox";
+            this.gamesListBox.Size = new System.Drawing.Size(205, 212);
+            this.gamesListBox.TabIndex = 1;
+            // 
+            // playersLabel
+            // 
+            this.playersLabel.AutoSize = true;
+            this.playersLabel.Location = new System.Drawing.Point(9, 38);
+            this.playersLabel.Name = "playersLabel";
+            this.playersLabel.Size = new System.Drawing.Size(53, 16);
+            this.playersLabel.TabIndex = 2;
+            this.playersLabel.Text = "Players";
+            // 
+            // gamesLabel
+            // 
+            this.gamesLabel.AutoSize = true;
+            this.gamesLabel.Location = new System.Drawing.Point(145, 38);
+            this.gamesLabel.Name = "gamesLabel";
+            this.gamesLabel.Size = new System.Drawing.Size(96, 16);
+            this.gamesLabel.TabIndex = 3;
+            this.gamesLabel.Text = "Current Games";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(229, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 288);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 29);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Add Player";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(121, 288);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 29);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Edit Player";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AdminForm";
+            this.ClientSize = new System.Drawing.Size(365, 336);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gamesLabel);
+            this.Controls.Add(this.playersLabel);
+            this.Controls.Add(this.gamesListBox);
+            this.Controls.Add(this.playersListBox);
+            this.Name = "AdminForm";
+            this.Text = "Admin Console";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox playersListBox;
+        private System.Windows.Forms.ListBox gamesListBox;
+        private System.Windows.Forms.Label playersLabel;
+        private System.Windows.Forms.Label gamesLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
